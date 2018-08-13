6dd7c4224fa856d5e1e3ac7f2db496b6277f0d72
Thomas Modeneis <thomas.modeneis@gmail.com>
2018-08-13 20:22:56 +0200

    deploy to prod

--- app/util/searchutils.js
+++ app/util/searchutils.js
@@ -2,7 +2,9 @@ export function getSearchHost (type) {
     let host = ''
     switch (type) {
         case 'news':
-            host = 'http://localhost:9001/elk/model';
+            // host = 'http://localhost:9001/elk/model';
+            host = 'https://apis.comentarismo.com/elk/model'
+
             // host = 'https://apis.comentarismo.com/elk/_all/commentaries'
             break
         case 'product':
@@ -10,7 +12,9 @@ export function getSearchHost (type) {
             host = 'https://apis.comentarismo.com/elk/_all/commentaries_product'
             break
         default:
-            host = 'http://localhost:9001/elk/model';
+            // host = 'http://localhost:9001/elk/model';
+            host = 'https://apis.comentarismo.com/elk/model'
+
             // searchOnTypeUrl = 'http://localhost:9000/elk/_all/';
             // host = 'https://apis.comentarismo.com/elk/_all/'
             break
